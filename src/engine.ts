7947ef3b1bb09bc6e1f673cd64258b3555190a6f
bodiddlie <bodiddlie@gmail.com>
2022-07-17 21:03:30 -0700

    refactor action to not have engine as parameter

--- src/engine.ts
+++ src/engine.ts
@@ -63,7 +63,7 @@ export class Engine {
     const action = handleInput(event);
 
     if (action) {
-      action.perform(this, this.player);
+      action.perform(this.player);
     }
 
     this.handleEnemyTurns();
--- src/input-handler.ts
+++ src/input-handler.ts
@@ -1,47 +1,49 @@
-import { Engine } from './engine';
 import { Entity } from './entity';
 
 export interface Action {
-  perform: (engine: Engine, entity: Entity) => void;
+  perform: (entity: Entity) => void;
 }
 
 export abstract class ActionWithDirection implements Action {
   constructor(public dx: number, public dy: number) {}
 
-  perform(_engine: Engine, _entity: Entity) {}
+  perform(_entity: Entity) {}
 }
 
 export class MovementAction extends ActionWithDirection {
-  perform(engine: Engine, entity: Entity) {
+  perform(entity: Entity) {
     const destX = entity.x + this.dx;
     const destY = entity.y + this.dy;
 
-    if (!engine.gameMap.isInBounds(destX, destY)) return;
-    if (!engine.gameMap.tiles[destY][destX].walkable) return;
-    if (engine.gameMap.getBlockingEntityAtLocation(destX, destY)) return;
+    if (!window.engine.gameMap.isInBounds(destX, destY)) return;
+    if (!window.engine.gameMap.tiles[destY][destX].walkable) return;
+    if (window.engine.gameMap.getBlockingEntityAtLocation(destX, destY)) return;
     entity.move(this.dx, this.dy);
   }
 }
 
 export class BumpAction extends ActionWithDirection {
-  perform(engine: Engine, entity: Entity) {
+  perform(entity: Entity) {
     const destX = entity.x + this.dx;
     const destY = entity.y + this.dy;
 
-    if (engine.gameMap.getBlockingEntityAtLocation(destX, destY)) {
-      return new MeleeAction(this.dx, this.dy).perform(engine, entity);
+    if (window.engine.gameMap.getBlockingEntityAtLocation(destX, destY)) {
+      return new MeleeAction(this.dx, this.dy).perform(entity);
     } else {
-      return new MovementAction(this.dx, this.dy).perform(engine, entity);
+      return new MovementAction(this.dx, this.dy).perform(entity);
     }
   }
 }
 
 export class MeleeAction extends ActionWithDirection {
-  perform(engine: Engine, entity: Entity) {
+  perform(entity: Entity) {
     const destX = entity.x + this.dx;
     const destY = entity.y + this.dy;
 
-    const target = engine.gameMap.getBlockingEntityAtLocation(destX, destY);
+    const target = window.engine.gameMap.getBlockingEntityAtLocation(
+      destX,
+      destY,
+    );
 
     if (!target) return;
 
